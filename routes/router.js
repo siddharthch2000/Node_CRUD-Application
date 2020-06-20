77f1b0dfe201f66760dbb958d9917974143514ba
sidjaat0073 <chodharysiddharth8@gmail.com>
2020-06-20 23:23:20 +0530

    Initial

---
+++ routes/router.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const router = express.Router();
+const Products = require('../model/Product');
+
+
+
+router.get('/Products',(req,res)=>{
+    res.render('Products');
+});
+
+router.get('/Create',(req,res)=>{
+    res.render('Create');
+});
+
+router.get('/Delete',(req,res)=>{
+    res.render('Delete');
+});
+
+router.get('/Edit',(req,res)=>{
+    res.render('Edit');
+});
+
+router.get('/List',(req,res)=>{
+    Products.find({}).exec((err,products)=>{
+        if(err) throw err;
+    res.render('List',{"products": products});
+});
+});
+
+
+
+
+
+
+module.exports = router;
